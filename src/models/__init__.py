9c8e1e23b10d08b0f4405e4dc45ecdd81e0f94fe
vitortiger <ovitorcosta08@gmail.com>
2025-07-03 18:13:18 -0300

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+from src.models.user import User
+from src.models.telegram_bot import TelegramBot
+from src.models.campaign import Campaign
+from src.models.lead import TelegramLead
+from src.models.invite_link import InviteLink
